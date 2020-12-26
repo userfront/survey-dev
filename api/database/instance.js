4bb1bcac86f6c30eefb6403c29aab897ba7b32b3
Tyler <tyler@anymod.com>
2020-12-26 17:20:39 -0500

    Remove vars

--- api/database/instance.js
+++ api/database/instance.js
@@ -10,12 +10,12 @@ if (process.env.NODE_ENV === "production") {
   console.log(process.env.RDS_HOSTNAME);
   console.log(process.env.RDS_PORT);
   console.log(process.env.RDS_DIALECT);
-  process.env.DATABASE_NAME = process.env.RDS_DB_NAME;
-  process.env.DATABASE_USERNAME = process.env.RDS_USERNAME;
-  process.env.DATABASE_PASSWORD = process.env.RDS_PASSWORD;
-  process.env.DATABASE_HOST = process.env.RDS_HOSTNAME;
-  process.env.DATABASE_PORT = process.env.RDS_PORT;
-  process.env.DATABASE_DIALECT = "postgres";
+  // process.env.DATABASE_NAME = process.env.RDS_DB_NAME;
+  // process.env.DATABASE_USERNAME = process.env.RDS_USERNAME;
+  // process.env.DATABASE_PASSWORD = process.env.RDS_PASSWORD;
+  // process.env.DATABASE_HOST = process.env.RDS_HOSTNAME;
+  // process.env.DATABASE_PORT = process.env.RDS_PORT;
+  // process.env.DATABASE_DIALECT = "postgres";
 }
 
 const sequelize = new Sequelize(
@@ -23,7 +23,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
   {
-    dialect: process.env.DATABASE_DIALECT,
+    dialect: process.env.DATABASE_DIALECT || "postgres",
     host: process.env.DATABASE_HOST || "localhost",
     port: process.env.DATABASE_PORT || 5432,
   }
