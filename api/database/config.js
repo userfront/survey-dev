4466e7f9aeb2529859bfd5ca84c9408fcf06bc11
Tyler <tyler@anymod.com>
2020-12-26 16:25:00 -0500

    Use RDS vars

--- api/database/config.js
+++ api/database/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   production: {
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: process.env.DATABASE_DIALECT,
+    database: process.env.RDS_DB_NAME,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    dialect: "postgres",
   },
   development: {
     database: process.env.DATABASE_NAME,
--- api/database/instance.js
+++ api/database/instance.js
@@ -4,6 +4,16 @@ const modelsDirectory = path.join(__dirname, "../models");
 const { Sequelize } = require("sequelize");
 const db = {};
 
+if (process.env.NODE_ENV === "production") {
+  console.log("db", process.env.RDS_HOSTNAME);
+  process.env.DATABASE_NAME = process.env.RDS_DB_NAME;
+  process.env.DATABASE_USERNAME = process.env.RDS_USERNAME;
+  process.env.DATABASE_PASSWORD = process.env.RDS_PASSWORD;
+  process.env.DATABASE_HOST = process.env.RDS_HOSTNAME;
+  process.env.DATABASE_PORT = process.env.RDS_PORT;
+  process.env.DATABASE_DIALECT = "postgres";
+}
+
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME,
