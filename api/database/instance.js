bd0d19bc51f9d72436c005651e46650bfa09b449
Tyler <tyler@anymod.com>
2020-12-26 17:16:05 -0500

    Use RDS vars

--- api/database/instance.js
+++ api/database/instance.js
@@ -10,12 +10,12 @@ if (process.env.NODE_ENV === "production") {
   console.log(process.env.RDS_HOSTNAME);
   console.log(process.env.RDS_PORT);
   console.log(process.env.RDS_DIALECT);
-  // process.env.DATABASE_NAME = process.env.RDS_DB_NAME;
-  // process.env.DATABASE_USERNAME = process.env.RDS_USERNAME;
-  // process.env.DATABASE_PASSWORD = process.env.RDS_PASSWORD;
-  // process.env.DATABASE_HOST = process.env.RDS_HOSTNAME;
-  // process.env.DATABASE_PORT = process.env.RDS_PORT;
-  // process.env.DATABASE_DIALECT = "postgres";
+  process.env.DATABASE_NAME = process.env.RDS_DB_NAME;
+  process.env.DATABASE_USERNAME = process.env.RDS_USERNAME;
+  process.env.DATABASE_PASSWORD = process.env.RDS_PASSWORD;
+  process.env.DATABASE_HOST = process.env.RDS_HOSTNAME;
+  process.env.DATABASE_PORT = process.env.RDS_PORT;
+  process.env.DATABASE_DIALECT = "postgres";
 }
 
 const sequelize = new Sequelize(
