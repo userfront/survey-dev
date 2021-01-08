d98669320b0e3396fd147c80983c6de680997941
Tyler <tyler@anymod.com>
2021-01-07 16:07:56 -0800

    Adding questions

--- src/questions.js
+++ src/questions.js
@@ -39,6 +39,14 @@ const technologyChoices = [
   "Free tools",
 ];
 
+const industries = [];
+
+const numberSettings = {
+  type: "text",
+  inputType: "number",
+  min: 0,
+};
+
 const workerPage = {
   title: "Your work setup in 2020",
   description:
@@ -77,26 +85,20 @@ const workerPage = {
     },
     {
       title: "How did you find work in 2020?",
-      type: "panel",
-      name: "workerMarketingPanel",
-      elements: [
-        {
-          title: "Methods",
-          name: "workerMarketingMethods",
-          type: "checkbox",
-          colCount: 4,
-          hasOther: true,
-          choices: [
-            "Upwork",
-            "Referral from client",
-            "Referral from network",
-            "Repeat clients",
-            "Own website",
-            "Online advertising",
-            "Offline advertising",
-          ],
-        },
+      name: "workerMarketingMethods",
+      type: "checkbox",
+      colCount: 4,
+      hasOther: true,
+      choices: [
+        "Upwork",
+        "Repeat clients",
+        "Own website",
+        "Online advertising",
+        "Offline advertising",
+        "Referral from client",
+        "Referral from network",
       ],
+      choicesOrder: "random",
     },
     {
       title: "What technologies did you use in 2020?",
@@ -110,6 +112,7 @@ const workerPage = {
           hasOther: true,
           colCount: 4,
           choices: frontendTechnologies,
+          choicesOrder: "random",
         },
         {
           title: "Backend",
@@ -118,6 +121,7 @@ const workerPage = {
           colCount: 4,
           hasOther: true,
           choices: backendTechnologies,
+          choicesOrder: "random",
         },
         {
           title: "Platform",
@@ -126,6 +130,7 @@ const workerPage = {
           colCount: 4,
           hasOther: true,
           choices: platformTechnologies,
+          choicesOrder: "random",
         },
       ],
     },
@@ -140,58 +145,261 @@ const payPage = {
   name: "pay",
   questions: [
     {
-      title: "Earnings",
+      title:
+        "How much did you earn in 2020 from contract web projects, before tax?",
+      step: 100,
+      name: "payTotal",
+      placeHolder: "$ Amount you earned",
+      ...numberSettings,
+    },
+    {
+      title:
+        "What percentage of your total earnings in 2020 were from contract web projects?",
+      type: "dropdown",
+      name: "payPercentage",
+      colCount: 5,
+      choices: [
+        "100%",
+        "90%",
+        "80%",
+        "70%",
+        "60%",
+        "50%",
+        "40%",
+        "30%",
+        "20%",
+        "10%",
+        "0%",
+      ],
+    },
+    {
+      title: "How many clients did you have in 2020?",
+      name: "payClientCount",
+      placeHolder: "Number of clients",
+      ...numberSettings,
+    },
+    {
+      title: "How many contracts did you have in 2020?",
+      name: "payContractCount",
+      placeHolder: "Number of contracts",
+      ...numberSettings,
+    },
+    {
+      title: "Did you have difficulty getting paid by any clients in 2020?",
+      type: "boolean",
+      name: "payDifficulty",
+    },
+  ],
+};
+
+const topClientPage = {
+  title: "Your top paying client",
+  description: "The client you earned the most from in 2020",
+  navigationTitle: "Top client",
+  name: "topClient",
+  questions: [
+    {
+      title:
+        "How much did you earn from your top paying client in 2020, before tax?",
+      placeHolder: "$ Amount you earned",
+      name: "topClientEarnings",
+      step: 100,
+      ...numberSettings,
+    },
+    {
+      title:
+        "How many separate contracts did you work on for your top paying client in 2020?",
+      placeHolder: "Number of contracts",
+      name: "topClientNumberOfContracts",
+      ...numberSettings,
+    },
+    {
+      title:
+        "How long was the longest contract for your top paying client in 2020?",
+      type: "dropdown",
+      name: "topClientLength",
+      choices: [
+        "Less than a week",
+        "1-4 weeks",
+        "1 month",
+        "2 months",
+        "3 months",
+        "4 months",
+        "5 months",
+        "6 months",
+        "More than 6 months",
+      ],
+    },
+    {
+      title:
+        "What tools did you personally use on the frontend for this client?",
+      type: "checkbox",
+      name: "topClientFrontend",
+      hasOther: true,
+      colCount: 4,
+      choices: frontendTechnologies,
+      choicesOrder: "random",
+    },
+    {
+      title:
+        "What tools did you personally use on the backend for this client?",
+      type: "checkbox",
+      name: "topClientBackend",
+      colCount: 4,
+      hasOther: true,
+      choices: backendTechnologies,
+      choicesOrder: "random",
+    },
+    {
+      title: "What platform(s) does this client use?",
+      type: "checkbox",
+      name: "topClientInfra",
+      colCount: 4,
+      hasOther: true,
+      choices: platformTechnologies,
+      choicesOrder: "random",
+    },
+    {
+      title:
+        "Which technology choices did you influence at your top paying client?",
+      type: "checkbox",
+      name: "topClientTechnology",
+      colCount: 3,
+      hasOther: true,
+      choices: technologyChoices,
+      choicesOrder: "random",
+    },
+    {
+      title: "Top client location",
       type: "panel",
-      name: "payPanel",
+      name: "topClientLocation",
       elements: [
         {
-          title:
-            "How much did you earn in 2020 from contract web projects, before tax?",
-          type: "text",
-          inputType: "number",
-          name: "payTotal",
-          placeHolder: "$",
-        },
-        {
-          title:
-            "What percentage of your total earnings in 2020 were from contract web projects?",
+          title: "Your top client's region",
           type: "dropdown",
-          name: "payPercentage",
-          colCount: 5,
-          choices: [
-            "100%",
-            "90%",
-            "80%",
-            "70%",
-            "60%",
-            "50%",
-            "40%",
-            "30%",
-            "20%",
-            "10%",
-            "0%",
-          ],
+          name: "topClientRegion",
+          choices: ["Africa", "Americas", "Asia", "Europe", "Oceania"],
         },
         {
-          title: "How many clients did you have in 2020?",
-          type: "text",
-          inputType: "number",
-          name: "payClientCount",
-          placeHolder: "Number of clients",
-        },
-        {
-          title: "How many contracts did you have in 2020?",
-          type: "text",
-          inputType: "number",
-          name: "payContractCount",
-          placeHolder: "Number of contracts",
-        },
-        {
-          title: "Did you have difficulty getting paid by any clients in 2020?",
-          type: "boolean",
-          name: "payDifficulty",
+          title: "Your top client's country",
+          type: "dropdown",
+          name: "topClientCountry",
+          choicesByUrl: {
+            url: "https://restcountries.eu/rest/v2/region/{topClientRegion}",
+            valueName: "name",
+          },
         },
       ],
+    },
+    {
+      title: "What industry is this client in?",
+      type: "dropdown",
+      name: "topClientIndustry",
+    },
+    {
+      title: "How many employees work at your top paying client?",
+      type: "dropdown",
+      name: "topClientEmployeeCount",
+      choices: ["1", "2-19", "20-99", "100-999", "1000 or more"],
+    },
+    {
+      title: "Did you have difficulty getting paid by this client?",
+      type: "boolean",
+      name: "topClientPayDifficulty",
+    },
+  ],
+};
+
+const bottomClient = {
+  title: "Your bottom paying client",
+  type: "panel",
+  name: "workBottomClientPanel",
+  description: "The client you earned the least from in 2020",
+  visibleIf: "{workClientCount} > 1",
+  questions: [
+    {
+      title:
+        "How much did you earn from your bottom paying client in 2020, before tax?",
+      type: "text",
+      inputType: "number",
+      placeHolder: "$ Amount you earned",
+      name: "workBottomClientEarnings",
+    },
+    {
+      title:
+        "How many separate contracts did you work on for your bottom paying client in 2020?",
+      type: "text",
+      inputType: "number",
+      placeHolder: "Number of contracts",
+      name: "workBottomClientNumberOfContracts",
+    },
+    {
+      title:
+        "How long was the longest contract for your bottom paying client in 2020?",
+      type: "dropdown",
+      name: "workBottomClientLength",
+      choices: [
+        "Less than a week",
+        "1-4 weeks",
+        "1 month",
+        "2 months",
+        "3 months",
+        "4 months",
+        "5 months",
+        "6 months",
+        "More than 6 months",
+      ],
+    },
+    {
+      title:
+        "What tools did you personally use on the frontend for this client?",
+      type: "checkbox",
+      name: "workBottomClientFrontend",
+      hasOther: true,
+      colCount: 4,
+      choices: frontendTechnologies,
+    },
+    {
+      title:
+        "What tools did you personally use on the backend for this client?",
+      type: "checkbox",
+      name: "workBottomClientBackend",
+      colCount: 4,
+      hasOther: true,
+      choices: backendTechnologies,
+    },
+    {
+      title: "What platform(s) does this client use?",
+      type: "checkbox",
+      name: "workBottomClientInfra",
+      colCount: 4,
+      hasOther: true,
+      choices: platformTechnologies,
+    },
+    {
+      title:
+        "Which technology choices did you influence at your bottom paying client?",
+      type: "checkbox",
+      name: "workBottomClientTechnology",
+      colCount: 3,
+      hasOther: true,
+      choices: technologyChoices,
+    },
+    {
+      title: "What industry is this client in?",
+      type: "dropdown",
+      name: "workBottomClientIndustry",
+    },
+    {
+      title: "How many employees work at your bottom paying client?",
+      type: "dropdown",
+      name: "workBottomClientEmployeeCount",
+      choices: ["1", "2-19", "20-99", "100-999", "1000 or more"],
+    },
+    {
+      title: "Did you have difficulty getting paid by this client?",
+      type: "boolean",
+      name: "workBottomClientPayDifficulty",
     },
   ],
 };
@@ -200,237 +408,7 @@ const questions = {
   // showPageNumbers: false,
   showProgressBar: "top",
   progressBarType: "buttons",
-  pages: [
-    workerPage,
-    payPage,
-    {
-      title: "Your work in 2020",
-      description:
-        "These questions help determine pay trends across clients and contracts.",
-      navigationTitle: "Top client",
-      name: "clients",
-      questions: [
-        {
-          type: "html",
-          name: "info",
-          html: "<br>",
-        },
-        {
-          title: "Your top paying client",
-          type: "panel",
-          name: "workTopClientPanel",
-          description: "The client you earned the most from in 2020",
-          elements: [
-            {
-              title:
-                "How much did you earn from your top paying client in 2020, before tax?",
-              type: "text",
-              inputType: "number",
-              placeHolder: "$ Amount you earned",
-              name: "workTopClientEarnings",
-            },
-            {
-              title:
-                "How many separate contracts did you work on for your top paying client in 2020?",
-              type: "text",
-              inputType: "number",
-              placeHolder: "Number of contracts",
-              name: "workTopClientNumberOfContracts",
-            },
-            {
-              title:
-                "How long was the longest contract for your top paying client in 2020?",
-              type: "dropdown",
-              name: "workTopClientLength",
-              choices: [
-                "Less than a week",
-                "1-4 weeks",
-                "1 month",
-                "2 months",
-                "3 months",
-                "4 months",
-                "5 months",
-                "6 months",
-                "More than 6 months",
-              ],
-            },
-            {
-              title:
-                "What tools did you personally use on the frontend for this client?",
-              type: "checkbox",
-              name: "workTopClientFrontend",
-              hasOther: true,
-              colCount: 4,
-              choices: frontendTechnologies,
-            },
-            {
-              title:
-                "What tools did you personally use on the backend for this client?",
-              type: "checkbox",
-              name: "workTopClientBackend",
-              colCount: 4,
-              hasOther: true,
-              choices: backendTechnologies,
-            },
-            {
-              title: "What platform(s) does this client use?",
-              type: "checkbox",
-              name: "workTopClientInfra",
-              colCount: 4,
-              hasOther: true,
-              choices: platformTechnologies,
-            },
-            {
-              title:
-                "Which technology choices did you influence at your top paying client?",
-              type: "checkbox",
-              name: "workTopClientTechnology",
-              colCount: 3,
-              hasOther: true,
-              choices: technologyChoices,
-            },
-            {
-              title: "Top client location",
-              type: "panel",
-              name: "workTopClientLocation",
-              elements: [
-                {
-                  title: "Your top client's region",
-                  type: "dropdown",
-                  name: "workTopClientRegion",
-                  choices: ["Africa", "Americas", "Asia", "Europe", "Oceania"],
-                },
-                {
-                  title: "Your top client's country",
-                  type: "dropdown",
-                  name: "workTopClientCountry",
-                  choicesByUrl: {
-                    url:
-                      "https://restcountries.eu/rest/v2/region/{workerRegion}",
-                    valueName: "name",
-                  },
-                },
-              ],
-            },
-            {
-              title: "What industry is this client in?",
-              type: "dropdown",
-              name: "workTopClientIndustry",
-            },
-            {
-              title: "How many employees work at your top paying client?",
-              type: "dropdown",
-              name: "workTopClientEmployeeCount",
-              choices: ["1", "2-19", "20-99", "100-999", "1000 or more"],
-            },
-            {
-              title: "Did you have difficulty getting paid by this client?",
-              type: "boolean",
-              name: "workTopClientPayDifficulty",
-            },
-          ],
-        },
-        {
-          type: "html",
-          name: "info",
-          html: "<br>",
-        },
-        {
-          title: "Your bottom paying client",
-          type: "panel",
-          name: "workBottomClientPanel",
-          description: "The client you earned the least from in 2020",
-          visibleIf: "{workClientCount} > 1",
-          elements: [
-            {
-              title:
-                "How much did you earn from your bottom paying client in 2020, before tax?",
-              type: "text",
-              inputType: "number",
-              placeHolder: "$ Amount you earned",
-              name: "workBottomClientEarnings",
-            },
-            {
-              title:
-                "How many separate contracts did you work on for your bottom paying client in 2020?",
-              type: "text",
-              inputType: "number",
-              placeHolder: "Number of contracts",
-              name: "workBottomClientNumberOfContracts",
-            },
-            {
-              title:
-                "How long was the longest contract for your bottom paying client in 2020?",
-              type: "dropdown",
-              name: "workBottomClientLength",
-              choices: [
-                "Less than a week",
-                "1-4 weeks",
-                "1 month",
-                "2 months",
-                "3 months",
-                "4 months",
-                "5 months",
-                "6 months",
-                "More than 6 months",
-              ],
-            },
-            {
-              title:
-                "What tools did you personally use on the frontend for this client?",
-              type: "checkbox",
-              name: "workBottomClientFrontend",
-              hasOther: true,
-              colCount: 4,
-              choices: frontendTechnologies,
-            },
-            {
-              title:
-                "What tools did you personally use on the backend for this client?",
-              type: "checkbox",
-              name: "workBottomClientBackend",
-              colCount: 4,
-              hasOther: true,
-              choices: backendTechnologies,
-            },
-            {
-              title: "What platform(s) does this client use?",
-              type: "checkbox",
-              name: "workBottomClientInfra",
-              colCount: 4,
-              hasOther: true,
-              choices: platformTechnologies,
-            },
-            {
-              title:
-                "Which technology choices did you influence at your bottom paying client?",
-              type: "checkbox",
-              name: "workBottomClientTechnology",
-              colCount: 3,
-              hasOther: true,
-              choices: technologyChoices,
-            },
-            {
-              title: "What industry is this client in?",
-              type: "dropdown",
-              name: "workBottomClientIndustry",
-            },
-            {
-              title: "How many employees work at your bottom paying client?",
-              type: "dropdown",
-              name: "workBottomClientEmployeeCount",
-              choices: ["1", "2-19", "20-99", "100-999", "1000 or more"],
-            },
-            {
-              title: "Did you have difficulty getting paid by this client?",
-              type: "boolean",
-              name: "workBottomClientPayDifficulty",
-            },
-          ],
-        },
-      ],
-    },
-  ],
+  pages: [workerPage, payPage, topClientPage],
 };
 
 export default questions;
